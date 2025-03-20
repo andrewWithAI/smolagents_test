6707df0728c317f5c1538ec3038b2d6b2299b5db
A. Thompson I <act.webdesigns@gmail.com>
2025-03-19 21:36:44 -0500

    completed test

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
 model = HfApiModel()
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
 
-agent.run("Look up the current prices web developers charge in the DFW area, then create a table of price ranges that could be used as a starting point for negotiations?")
+agent.run(
+    "Look up the current prices web developers charge in the DFW area, then create a table of price ranges that could be used as a starting point for negotiations?"
+)
